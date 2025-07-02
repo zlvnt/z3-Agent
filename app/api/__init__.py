d9f673a5fe14cbe5b23757a27f2e3f2001125e73
zx <zlvyynnnn@gmail.com>
2025-07-02 13:16:37 +0700

    __init__  draft

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 from .webhook import router as webhook_router
 
 router = APIRouter()
-router.include_router(webhook_router, prefix="/webhook", tags=["webhook"])
+router.include_router(webhook_router, prefix="/webhook", tags=["instagram"])
 
 __all__ = ["router"]
