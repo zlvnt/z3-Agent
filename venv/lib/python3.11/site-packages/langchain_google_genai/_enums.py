1bfa9fb313fe4a5ca51ca94a8990567b667df754
zx <zlvyynnnn@gmail.com>
2025-07-15 11:37:41 +0700

    blunder

--- venv/lib/python3.11/site-packages/langchain_google_genai/_enums.py
+++
@@ -1,7 +0,0 @@
-import google.ai.generativelanguage_v1beta as genai
-
-HarmBlockThreshold = genai.SafetySetting.HarmBlockThreshold
-HarmCategory = genai.HarmCategory
-Modality = genai.GenerationConfig.Modality
-
-__all__ = ["HarmBlockThreshold", "HarmCategory", "Modality"]
